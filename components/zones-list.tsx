3d6f4721b122359ebf7a77b97215bfbfda85770a
Bruno Trinitario <129905900+BrunoTrinitario@users.noreply.github.com>
2025-06-23 12:06:22 -0300

    initial commit add zone feature

--- components/zones-list.tsx
+++ components/zones-list.tsx
@@ -1,13 +1,44 @@
 "use client"
-
+import { Button } from "@/components/ui/button"
 import { useCattle } from "@/lib/cattle-context"
+import { Plus } from "lucide-react"
+import { useState } from "react"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { Textarea } from "@/components/ui/textarea"
+
 
 interface ZonesListProps {
   onItemClick?: () => void
 }
 
 export default function ZonesList({ onItemClick }: ZonesListProps) {
+  const [isAddDialogOpen, setIsAddDialogOpen] = useState(false);
   const { zones, cattle, selectedZoneId, setSelectedZoneId } = useCattle()
+  const [newZone, setNewZone] = useState({
+    name: "",
+    description: "",
+    x1: 0,
+    y1: 0,
+    x2: 0,
+    y2: 0,
+  });
 
   // Contar vacas por zona
   const cattleCountByZone = zones.reduce(
@@ -40,6 +71,83 @@ export default function ZonesList({ onItemClick }: ZonesListProps) {
           </span>
         </button>
       ))}
+      <Button onClick={() => setIsAddDialogOpen(true)} size="sm" variant="default" className="w-full">
+        <Plus className="h-4 w-4 mr-1" />
+        Agregar
+      </Button>
+      <Dialog open={isAddDialogOpen} onOpenChange={setIsAddDialogOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Agregar nueva zona</DialogTitle>
+            <DialogDescription>
+              Completa el formulario para agregar una nueva zona al sistema.
+            </DialogDescription>
+          </DialogHeader>
+
+          <div className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="name">Nombre *</Label>
+              <Input
+                onChange={(e) => setNewZone({ ...newZone, name: e.target.value })}
+              />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="description">Descripción *</Label>
+              <Textarea
+                onChange={(e) => setNewZone({ ...newZone, description: e.target.value })}
+
+              />
+            </div>
+
+            <div className="space-y-3">
+              <Label htmlFor="description">Primer limite X Y </Label>
+              <Input 
+                type="number" 
+                step="any"
+                id="x1"
+                placeholder="Coordenadas en x 1"
+                onChange={(e) => setNewZone({ ...newZone, x1: parseFloat(e.target.value) })}
+              />
+              <Input 
+                type="number"
+                step="any" 
+                id="y1"
+                placeholder="Coordenadas en y 1"
+                onChange={(e) => setNewZone({ ...newZone, y1: parseFloat(e.target.value) })}
+              />
+            </div>
+
+            <div className="space-y-4">
+              <Label htmlFor="description">Segundo limite X Y </Label>
+              <Input 
+                type="number"
+                step="any" 
+                id="x2"
+                placeholder="Coordenadas en x 2"
+                onChange={(e) => setNewZone({ ...newZone, x2: parseFloat(e.target.value) })}
+              />
+              <Input 
+                type="number"
+                step="any" 
+                id="y2"
+                placeholder="Coordenadas en y 2"
+                onChange={(e) => setNewZone({ ...newZone, y2: parseFloat(e.target.value) })}
+              />
+            </div>
+          </div>
+                
+          <DialogFooter>
+            <Button variant="outline" onClick={() => setIsAddDialogOpen(false)}>
+              Cancelar
+            </Button>
+            <Button variant="outline">
+                Guardar
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </div>
   )
 }
+
